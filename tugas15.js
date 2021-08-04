8ec4d20bb3bbd52a0946d5eb74c0f2867bc5e671
Ariq Fikri Tamamy <ariqrick@gmail.com>
2021-08-04 19:02:10 +0700

    Tugas 15 Javascript

---
+++ tugas15.js
@@ -0,0 +1,10 @@
+function lebarSegitiga(){
+  var AB = 8
+  var BC = 6
+  console.log("Nilai AB =",AB)
+  console.log("Nilai BC =",BC)
+  var AC = Math.sqrt((Math.pow(AB,2)+Math.pow(BC,2)))
+  console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah :",AC,"cm")
+}
+
+lebarSegitiga()
